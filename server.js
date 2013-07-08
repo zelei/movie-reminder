43a2fb887bcfe9ef952cf8e98fbd4dc870331ae1
OpenShift guest <sandor.zelei@gmail.com>
2013-07-08 15:22:12 -0400

    Fixed version of application

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ context.app.get('/search', context.require("/server/controller/SearchController"
 
 context.app.get('/upcoming', context.require("/server/controller/UpcomingController"));
 
-context.app.listen(process.env.PORT, process.env.IP);
+context.app.listen(process.env.PORT || '8080', process.env.IP || '127.0.0.1' );
 console.log('Listening on port ' + process.env.IP);
