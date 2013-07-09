953d2388522a08bd5d1e5fc42d2364b2d1b4a70a
OpenShift guest <sandor.zelei@gmail.com>
2013-07-09 01:42:50 -0400

    Fixed port and address value of variables

--- server.js
+++ server.js
@@ -6,5 +6,11 @@ context.app.get('/search', context.require("/server/controller/SearchController"
 
 context.app.get('/upcoming', context.require("/server/controller/UpcomingController"));
 
-context.app.listen(process.env.PORT || '8080', process.env.IP || '127.0.0.1' );
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+//process.env.PORT
+//process.env.IP
+
+context.app.listen(ipaddr, port);
 console.log('Listening on port ' + process.env.IP);
