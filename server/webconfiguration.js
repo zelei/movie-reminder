f095b9a5ba98ece08f7cdc8368cd728a3e082a59
OpenShift guest <sandor.zelei@gmail.com>
2013-07-22 02:12:59 -0400

    Minor refactoring

--- server/webconfiguration.js
+++ server/webconfiguration.js
@@ -3,9 +3,36 @@ var express = require('express');
 var ect = require('ect');
 var logger = require('winston');
 var passport = require('passport');
+var User = env.require("/server/model/User");
 var GoogleStrategy = require('passport-google').Strategy;
 
-var User = env.require("/server/model/User");
+var templateDirectory = env.root + '/web/views';
+
+var app = express();
+
+app.configure(function() {
+  
+  app.set('json spaces', 0);
+  
+  // ECT
+  app.set('views', templateDirectory);
+  app.engine('.ect', ect({ watch: true, root: templateDirectory }).render);
+
+  // Express
+  app.use(express.compress());
+  app.use("/static", express.static(env.root + '/web/resources', { maxAge: 86400000 }));
+  app.use(express.logger());
+
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  
+  // Passport
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  app.use(app.router);  
+});
 
 passport.use(new GoogleStrategy({
     returnURL: env.host + '/auth/google/return',
@@ -23,32 +50,6 @@ passport.use(new GoogleStrategy({
         }
   
 ));
-
-var templateDirectory = env.root + '/web/views';
-
-var app = express();
-
-app.configure(function() {
-      
-  // ECT
-  app.set('views', templateDirectory);
-  app.engine('.ect', ect({ watch: true, root: templateDirectory }).render);
-
-  // Express
-  app.use(express.compress());
-  app.use("/static", express.static(env.root + '/web/resources', { maxAge: 86400000 }));
-  //app.use(express.logger());
-
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.session({ secret: 'keyboard cat' }));
-  
-  // Passport
-  app.use(passport.initialize());
-  app.use(passport.session());
-  
-  app.use(app.router);  
-});
 
 passport.serializeUser(function(user, done) {
     done(null, user);
