b7874f9c1390b1255eeac292cb6d9f56223b057a
OpenShift guest <sandor.zelei@gmail.com>
2013-08-02 16:32:55 -0400

    Fixed mongo db auth

--- server/service/Database.js
+++ server/service/Database.js
@@ -1,6 +1,15 @@
 var env = require("rekuire")("env");
 var mongoose = require("mongoose");
 
-var connection = mongoose.connect('mongodb://' + env.mongo.url + '/' + env.mongo.name);
+var options = {
+  server: { poolSize: 5 }
+};
+
+if(env.mongo.user && env.mongo.password) {
+    options.user = env.mongo.user;
+    options.pass = env.mongo.password;
+}
+
+var connection = mongoose.connect('mongodb://' + env.mongo.url + '/' + env.mongo.name, options);
 
 module.exports = {'mongoose' : mongoose, 'connection' : connection};
